dd1a7d2e81b2f132070a9a49db3767d422fddfbd
vstep <vstepenko@alliancedigital.tech>
2026-03-06 15:00:33 +0200

    refactor: DocumentController to use DTOs

--- src/main/java/com/company/is/controller/DocumentController.java
+++ src/main/java/com/company/is/controller/DocumentController.java
@@ -1,33 +1,44 @@
 package com.company.is.controller;
 
-import com.company.is.domain.Document;
+import com.company.is.dto.CreateDocumentRequest;
+import com.company.is.dto.DocumentResponse;
+import com.company.is.dto.UpdateDocumentRequest;
 import com.company.is.service.DocumentService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/documents")
+@RequiredArgsConstructor
 public class DocumentController {
 
-    @Autowired
-    private DocumentService documentService;
+    private final DocumentService documentService;
 
     @PostMapping
-    public Document createDocument(@RequestBody Document document) {
-        return documentService.createDocument(document);
+    public DocumentResponse createDocument(@RequestBody @Valid CreateDocumentRequest request) {
+        return documentService.createDocument(request);
     }
 
-    @org.springframework.web.bind.annotation.PutMapping("/{id}")
-    public Document updateDocument(@org.springframework.web.bind.annotation.PathVariable Long id, @RequestBody Document document) {
-        return documentService.updateDocument(id, document);
+    @PutMapping("/{id}")
+    public DocumentResponse updateDocument(@PathVariable Long id, @RequestBody @Valid UpdateDocumentRequest request) {
+        return documentService.updateDocument(id, request);
     }
 
-    @org.springframework.web.bind.annotation.DeleteMapping("/{id}")
+    @DeleteMapping("/{id}")
     public void deleteDocument(@PathVariable Long id) {
         documentService.deleteDocument(id);
+    }
+
+    @GetMapping("/{id}")
+    public DocumentResponse getDocument(@PathVariable Long id) {
+        return documentService.getDocument(id);
     }
 }
